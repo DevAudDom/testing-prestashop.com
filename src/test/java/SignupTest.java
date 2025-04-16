2c0c350a28526e680dc49169c534dae66f1681f0
LilSebasXVII <117200692+LilSebasXVII@users.noreply.github.com>
2025-04-16 15:45:05 -0400

    This is the website ive been working on (https://demo.prestashop.com/#/en/front)

--- src/test/java/SignupTest.java
+++ src/test/java/SignupTest.java
@@ -2,66 +2,94 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
 
 public class SignupTest {
     WebDriver chromeDriver;
 
     @BeforeTest
-    public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "C:/Users/domlu/OneDrive - Florida Gulf Coast University/Spring 2025/ST/Installations/chromedriver-win64/chromedriver-win64/chromedriver.exe");
-        chromeDriver = new ChromeDriver(); // why new?
-        chromeDriver.manage().window().maximize();
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "C:/Users/anton/Downloads/chromedriver-win64/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.setCapability("unhandledPromptBehavior", "accept"); // Accept all alerts automatically (cookies or some popup)
+        chromeDriver = new ChromeDriver();
+        chromeDriver.manage().window().maximize(); // Maximize the window
     }
 
-    @Test ()
-    public void enterNewEmail() throws InterruptedException {
-        chromeDriver.get("https://www.spotify.com/us/signup?forward_url=https%3A%2F%2Fopen.spotify.com%2F");
+    @org.testng.annotations.Test
+    public void login() throws InterruptedException {
+        chromeDriver.get("https://demo.prestashop.com/#/en/front");
+        JavascriptExecutor js = (JavascriptExecutor) chromeDriver;
+        WebDriverWait wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(20));
+        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.id("framelive")));
 
-        WebElement emailAddress = chromeDriver.findElement(By.id("username"));
-        emailAddress.sendKeys("pronoodlesoup@gmail.com");
-        WebElement nextButton = chromeDriver.findElement(By.cssSelector(".Button-sc-qlcn5g-0.VsdHm.encore-text-body-medium-bold"));
-        Thread.sleep(2000); // button seems not to get pressed unless we wait before
-        nextButton.click();}
-    @Test ()
-    public void enterNewPassword() throws InterruptedException {
-        Thread.sleep(2000); // wait for page to load
-        WebElement newPassword = chromeDriver.findElement(By.id("new-password"));
-        newPassword.sendKeys("123456789a");
-        WebElement nextButton = chromeDriver.findElement(By.cssSelector(".Button-sc-qlcn5g-0.VsdHm.encore-text-body-medium-bold"));
-        Thread.sleep(2000);
-        nextButton.click();
-    }
-    @Test ()
-    public void enterStep2Signup() throws InterruptedException {
-        Thread.sleep(2000); // wait for page to load
-        WebElement newName = chromeDriver.findElement(By.id("displayName"));
-        newName.sendKeys("test name");
-        WebElement DOBMonth = chromeDriver.findElement(By.id("month"));
-        // use seleniums special select object and pass in element
-        Select monthDD = new Select(DOBMonth);
-        Thread.sleep(2000);
-        monthDD.selectByValue("1");
-        WebElement DOBDay = chromeDriver.findElement(By.id("day"));
-        DOBDay.sendKeys("01");
-        WebElement DOBYear = chromeDriver.findElement(By.id("year"));
-        DOBYear.sendKeys("2000");
-        WebElement gender = chromeDriver.findElement(By.xpath("//span[text()='Something else']"));
-        gender.click();
-        WebElement nextButton = chromeDriver.findElement(By.cssSelector(".Button-sc-qlcn5g-0.VsdHm.encore-text-body-medium-bold"));
-        Thread.sleep(2000); // button seems not to get pressed unless we wait before
-        nextButton.click();
-    }
 
-    @Test ()
-    public void enterStep3Signup() throws InterruptedException {
-        Thread.sleep(2000); // wait for page to laod
-        WebElement checkbox = chromeDriver.findElement(By.cssSelector(".Indicator-sc-1airx73-0.lhZnAn"));
+        WebElement signup = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#_desktop_user_info > div > a > span")));
+        signup.click();
+        Thread.sleep(1000);
+        WebElement createNew = chromeDriver.findElement(By.cssSelector("#content > div > a"));
+        createNew.click();
+        Thread.sleep(1000);
+        WebElement Gender = chromeDriver.findElement(By.id("field-id_gender-1"));
+        Gender.click();
+        Thread.sleep(1000);
+        WebElement FName = chromeDriver.findElement(By.id("field-firstname"));
+        FName.sendKeys("Jose");
+        Thread.sleep(1000);
+        WebElement LName = chromeDriver.findElement(By.id("field-lastname"));
+        LName.sendKeys("F");
+        Thread.sleep(1000);
+        js.executeScript("window.scrollBy(0,100)");
+        Thread.sleep(1000);
+        WebElement Email = chromeDriver.findElement(By.id("field-email"));
+        Email.sendKeys("antoniofdez2203@gmail.com");
+        Thread.sleep(1000);
+        WebElement Password = chromeDriver.findElement(By.id("field-password"));
+        Password.sendKeys("Jose5445!!");
+        Thread.sleep(1000);
+        js.executeScript("window.scrollBy(0,100)");
+        Thread.sleep(1000);
+        WebElement Birthdate = chromeDriver.findElement(By.id("field-birthday"));
+        Birthdate.sendKeys("05/26/1991");
+        Thread.sleep(1000);
+        WebElement checkbox = chromeDriver.findElement(By.cssSelector("#customer-form > div > div:nth-child(8) > div.col-md-6.js-input-column > span > label > input[type=checkbox]"));
         checkbox.click();
-        WebElement signUpButton = chromeDriver.findElement(By.cssSelector(".ButtonInner-sc-14ud5tc-0.hvvTXU.encore-bright-accent-set"));
-        Thread.sleep(2000); // button seems not to get pressed unless we wait before
-        signUpButton.click();
+        Thread.sleep(1000);
+        js.executeScript("window.scrollBy(0,200)");
+        Thread.sleep(1000);
+        WebElement checkbox2 = chromeDriver.findElement(By.cssSelector("#customer-form > div > div:nth-child(10) > div.col-md-6.js-input-column > span > label > input[type=checkbox]"));
+        checkbox2.click();
+        Thread.sleep(1000);
+        WebElement save = chromeDriver.findElement(By.cssSelector("#customer-form > footer > button"));
+        save.click();
+        Thread.sleep(1000);
+
+        chromeDriver.switchTo().defaultContent(); // get out of iframe
+        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.id("framelive"))); // get back into iframe
+
+        WebElement signOut = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#_desktop_user_info > div > a.logout.hidden-sm-down")));
+        signOut.click();
+        Thread.sleep(1000);
+
+        WebElement login = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#_desktop_user_info > div > a > span")));
+        login.click();
+        Thread.sleep(1000);
+        WebElement email = chromeDriver.findElement(By.id("field-email"));
+        email.sendKeys("antoniofdez2203@gmail.com");
+        Thread.sleep(1000);
+        WebElement pass = chromeDriver.findElement(By.id("field-password"));
+        Thread.sleep(1000);
+        pass.sendKeys("Jose5445!!");
+        Thread.sleep(1000);
+        WebElement loginButton = chromeDriver.findElement(By.id("submit-login"));
+        loginButton.click();
     }
 }
+
